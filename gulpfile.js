11b6f2df747e90f625195f1590b6f7b4ae439f91
Peter halloran <mustardbeard28@gmail.com>
2016-06-07 15:07:33 -0400

    setting up project

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp          = require('gulp');
+var concat        = require('gulp-concat');
+var uglify        = require('gulp-uglify');
+var rename        = require('gulp-rename');
+var sync          = require('run-sequence');
+var gutil         = require('gulp-util');
+var browser       = require('browser-sync');
